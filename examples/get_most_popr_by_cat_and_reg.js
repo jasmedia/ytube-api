5ba0b7a59025ac03c3ebcaa85154da7a55d395ee
jaseem <jasmedia.jas@gmail.com>
2020-07-28 22:08:04 +0530

    updated example

--- examples/get_most_popr_by_cat_and_reg.js
+++ examples/get_most_popr_by_cat_and_reg.js
@@ -4,13 +4,6 @@ const config = require('./config');
 const youTube = new YouTube();
 
 youTube.setKey(config.key);
-// youTube.getById('HcwTxRuq-uk,vIu85WQTPRc', (error, result) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(JSON.stringify(result, null, 2));
-//   }
-// });
 
 youTube.getMostPopularByCategoryAndRegion(2, 10, 'AU', function(error, result) {
     if (error) {
