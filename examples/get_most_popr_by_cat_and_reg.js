26eeab7f88eebc7712692bfb19ec794819f07fa3
jaseem <jasmedia.jas@gmail.com>
2020-07-29 10:55:59 +0530

    next page token implemented

--- examples/get_most_popr_by_cat_and_reg.js
+++ examples/get_most_popr_by_cat_and_reg.js
@@ -4,11 +4,32 @@ const config = require('./config');
 const youTube = new YouTube();
 
 youTube.setKey(config.key);
+let count = 0;
 
-youTube.getMostPopularByCategoryAndRegion(2, 10, 'AU', function(error, result) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(JSON.stringify(result, null, 2));
+function getVideos(category, region, token = null) {
+    if (token) {
+        youTube.setNextPageToken(token);
     }
-});
+    youTube.getMostPopularByCategoryAndRegion(50, category, region, function(error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(JSON.stringify(result.pageInfo));
+            // console.log(JSON.stringify(result, null, 2));
+            const videos = result.items;
+            count += videos.length;
+            if (result.nextPageToken) {
+                let token = result.nextPageToken;
+                console.log(token)
+                setTimeout(() => {
+                    getVideos(category, region, token);
+                }, 2000);
+            } else {
+                console.log('Finished, total videos', count);
+                count = 0;
+            }
+        }
+    });
+}
+
+getVideos(10, 'AU');
